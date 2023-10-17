089e6eb24423abfa1b5e59aa751306078f919cc2
Lu Dinh Quan <ludinhquan@gmail.com>
2023-10-17 15:23:47 +0700

    infra: setup deployment

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
   optimizeDeps: {
     include: ["@emotion/styled"],
   },
---
+++ build.dev.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+REGISTRY=demo.registry.io
+GCLOUD_REGISTRY=australia-southeast1-docker.pkg.dev/booking-system-386813/staging
+
+docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) -t $REGISTRY/server -f infra/docker/Dockerfile.server .
+docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) -t $REGISTRY/client -f infra/docker/Dockerfile.client .
+
+docker tag $REGISTRY/server $GCLOUD_REGISTRY/server
+docker tag $REGISTRY/client $GCLOUD_REGISTRY/client
+
+docker push $GCLOUD_REGISTRY/server
+docker push $GCLOUD_REGISTRY/client
